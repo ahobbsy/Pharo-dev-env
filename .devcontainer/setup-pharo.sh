0b481bbffa7cf52a6930468a7cbacad251644f05
ahobbsy <48050187+ahobbsy@users.noreply.github.com>
2026-08-21 12:56:21 +0000

    Initial PHaro image commit

--- .devcontainer/setup-pharo.sh
+++ .devcontainer/setup-pharo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "=== Downloading Pharo 64-bit Linux VM and Image ==="
-curl https://get.pharo.org/64/ | bash
+#curl https://get.pharo.org/64/ | bash
+wget -qO- https://get.pharo.org/64/150 | bash
 
 echo "=== Ensuring permissions are correct ==="
 chmod +x pharo
